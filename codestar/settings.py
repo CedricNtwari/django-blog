3ea049a31a45d0e138c97f7885ea79f42cdab41e
CedricNtwari <cedric.ntwari@powercoders.org>
2024-05-22 19:36:02 +0200

    Update static file settings for production deployment

--- codestar/settings.py
+++ codestar/settings.py
@@ -143,7 +143,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # This is where collectstatic will put your files
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ] # This is where your static files live in development
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
