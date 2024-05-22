1da96e24b00172b8d51d50a1011302da72d432c2
CedricNtwari <cedric.ntwari@powercoders.org>
2024-05-22 11:25:00 +0200

    Updating models

--- blog/models.py
+++ blog/models.py
@@ -13,6 +13,9 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.CharField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
+    field_1 = models.CharField()
+    field_2 = models.CharField(default="Hello World")
+    field_3 = models.CharField(null=True)
 
     class Meta:
         ordering = ["-created_on"]
